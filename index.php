9fdcdce96811d8f666031de98553c938466e6e77
webjhon <colorado.santos@gmail.com>
2015-10-09 19:20:58 -0300

    Organização dos Arquivos
    Organização do código já feito.

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Andre Prado</title>
+
+        <!-- jQuery -->
+        <script src="./js/jquery-1.11.3.min.js"></script>
+
+        <!-- Bootstrap -->
+        <script src="./js/bootstrap.min.js"></script>
+        <link href="./css/bootstrap.min.css" rel="stylesheet" media="all">
+        <link href="./css/bootstrap-responsive.min.css" rel="stylesheet" media="all">
+
+        <!--<script>
+            jQuery(document).ready(function($) {
+                $('#btnPesquisar').click(function(event) {
+                    console.log('Pesquisar por: ' + $('#iptPesquisar').val());
+                });
+            });
+        </script>-->
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row-fluid text-center">
+                <div class="offset2 span8 offset2">
+                    <legend>Busca Trabalhos</legend>
+                </div>
+            </div>
+            
+            <div class="row-fluid text-center">
+                <div class="offset2 span8 offset2">
+                    <form class="form-search">
+                        <div class="input-append">
+                            <input type="text" class="search-query" id="iptPesquisar">
+                            <button type="button" class="btn" id="btnPesquisar">Pesquisar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="row-fluid">
+                <div class="span12">
+                    <div class="row-fluid">
+                        <div class="offset2 span8 offset2">
+                            <hr>
+                            <dl class="dl-horizontal">
+                                <dt>Título</dt>
+                                <dd>Título do trabalho</dd>
+
+                                <dt>Autor</dt>
+                                <dd>Autor do trabalho</dd>
+
+                                <dt>Ano</dt>
+                                <dd>Ano do trabalho</dd>
+
+                                <dt>Descrição</dt>
+                                <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</dd>
+                            </dl>
+                        </div>
+                    </div>
+
+                    <div class="row-fluid text-center">
+                        <div class="offset2 span8 offset2">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-primary">Visualizar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row-fluid">
+                <div class="span12">
+                    <div class="row-fluid">
+                        <div class="offset2 span8 offset2">
+                            <hr>
+                            <dl class="dl-horizontal">
+                                <dt>Título</dt>
+                                <dd>Título do trabalho</dd>
+
+                                <dt>Autor</dt>
+                                <dd>Autor do trabalho</dd>
+
+                                <dt>Ano</dt>
+                                <dd>Ano do trabalho</dd>
+
+                                <dt>Descrição</dt>
+                                <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</dd>
+                            </dl>
+                        </div>
+                    </div>
+
+                    <div class="row-fluid text-center">
+                        <div class="offset2 span8 offset2">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-primary">Visualizar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row-fluid">
+                <div class="span12">
+                    <div class="row-fluid">
+                        <div class="offset2 span8 offset2">
+                            <hr>
+                            <dl class="dl-horizontal">
+                                <dt>Título</dt>
+                                <dd>Título do trabalho</dd>
+
+                                <dt>Autor</dt>
+                                <dd>Autor do trabalho</dd>
+
+                                <dt>Ano</dt>
+                                <dd>Ano do trabalho</dd>
+
+                                <dt>Descrição</dt>
+                                <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</dd>
+                            </dl>
+                        </div>
+                    </div>
+
+                    <div class="row-fluid text-center">
+                        <div class="offset2 span8 offset2">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-primary">Visualizar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
