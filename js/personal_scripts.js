4575f7c66fe86fae461af30dc9fb1f9c120cd820
webjhon <colorado.santos@gmail.com>
2015-10-09 20:58:23 -0300

    Correções boostrap
    Alterações no código e inserção  de linhas faltando.

---
+++ js/personal_scripts.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+    //-------------** SCRIPT PARA FUNCIONAMENTO DA BUSCA -- INICIO -------------** //
+    /*$('#btnPesquisar').click(function(event) {
+        console.log('Pesquisar por: ' + $('#iptPesquisar').val());
+    });*/
+    //-------------** SCRIPT PARA FUNCIONAMENTO DA BUSCA -- FINAL -------------** //
+    
+    
+    
+        
+});
+
+
