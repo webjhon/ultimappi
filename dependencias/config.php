536598f1733b5e6ab7332f165bfe791c7bbba797
webjhon <colorado.santos@gmail.com>
2015-10-27 21:22:29 -0200

    Home Estrutura
    Criação da estrutura básica, e reorganização dos arquivos.

---
+++ dependencias/config.php
@@ -0,0 +1,21 @@
+  <!-- AQUI FICAM TODOS OS ARQUIVOS NECESSÁRIOS PARA FUNCIONAMENTO DO BOOSTRAP -->
+  
+  <link href="../css/bootstrap.min.css" rel="stylesheet"> <!-- CSS Bootstrap -->
+  <script src="../js/jquery-2.1.4.min.js"></script> <!-- jQuery -->
+  <script src="../js/bootstrap.min.js"></script> <!-- Javascript Bootstrap -->
+  
+  
+  <!-- AQUI FICAM AS ALTERAÇÕES FEITAS MANUALMENTE NA PAGINA -->
+  
+  <link href="../css/custom.css" rel="stylesheet"> <!-- Alterações no CSS em Geral -->
+  <script src="../js/custom.js"></script> <!-- Alterações no JAVASCRIPT em Geral -->
+
+  
+
+
+  
+
+
+
+
+
