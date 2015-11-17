0f0ed5253bce3ca432e2a4c464673b5353f2dbfa
webjhon <colorado.santos@gmail.com>
2015-11-17 19:31:02 -0200

    Cadastro de Projetos - Alterações André
    - Cadstro de projetos com envio de arquivos implementado.
    
    - Alterações no banco para dar suporte ao cadastro (alterações não
    oficiais, aguardando as do spode)
    
    - Correções na página de cadastro.

--- backend/cadastro.php
+++ backend/cadastro.php
@@ -6,14 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once '../dependencias/config.php'; ?>
     <!-- Bootstrap -->
-
   </head>
+  <script src="../js/jquery-2.1.4.min.js"></script>
+  
+  <script>
+    jQuery(document).ready(function($) {
 
+        $('#arquivo1').bind('change', function() {
+
+          // this.files[0].size gets the size of your file.
+          //alert(this.files[0].size);
+		  var fileName = $(this).val();
+          var arrExt = fileName.split('.');
+		  var position = (arrExt.length)-1;
+		  var ext = arrExt[position];
+		  if(ext != 'pdf' || ext.length == 0 ){
+			$(this).val('');
+			alert('Precisa ser um pdf');
+		  }	     
+		});
+	  
+      });
+  </script>
 
   <body>
+	<?php
+	if ( isset($_GET['status']) ) {
+       echo "<script>alert('Cadastrado com Sucesso.');</script>";
+    }
 
+	?>
 
-    <form class="form-horizontal">
+    <form id='form1' class="form-horizontal" action="salvarCadastro.php" method="POST" enctype="multipart/form-data">
     <fieldset>
 
     <!-- Formulario de Cadastro de Projetos -->
@@ -23,7 +47,7 @@
     <div class="form-group">
       <label class="col-md-4 control-label" for="input_titulo">Título do Projeto</label>
       <div class="col-md-6">
-      <input id="input_titulo" name="input_titulo" type="text" placeholder="Digite aqui o título do projeto" class="form-control input-md" required="">
+      <input id="titulo" name="titulo" type="text" placeholder="Digite aqui o título do projeto" class="form-control input-md" required="">
 
       </div>
     </div>
@@ -32,7 +56,7 @@
     <div class="form-group">
       <label class="col-md-4 control-label" for="input_autores">Autor (es)</label>
       <div class="col-md-6">
-      <input id="input_autores" name="input_autores" type="text" placeholder="Ex.: TAL, Fulano de; TAL, Siclano de;" class="form-control input-md" required="">
+      <input id="autores" name="autores" type="text" placeholder="Ex.: TAL, Fulano de; TAL, Siclano de;" class="form-control input-md" required="">
       <span class="help-block">Separados por ( ; )</span>
       </div>
     </div>
@@ -41,7 +65,7 @@
     <div class="form-group">
       <label class="col-md-4 control-label" for="input_orientadores">Orientador (es)</label>
       <div class="col-md-6">
-      <input id="input_orientadores" name="input_orientadores" type="text" placeholder="Ex.: TAL, Fulano de; TAL, Siclano de;" class="form-control input-md" required="">
+      <input id="orientadores" name="orientadores" type="text" placeholder="Ex.: TAL, Fulano de; TAL, Siclano de;" class="form-control input-md" required="">
       <span class="help-block">Separados por ( ; )</span>
       </div>
     </div>
@@ -50,7 +74,7 @@
     <div class="form-group">
       <label class="col-md-4 control-label" for="input_keyword">Palavras-chave</label>
       <div class="col-md-5">
-      <input id="input_keyword" name="input_keyword" type="text" placeholder="exemplo; de; keyword;" class="form-control input-md" required="">
+      <input id="palavrachave" name="palavrachave" type="text" placeholder="exemplo; de; keyword;" class="form-control input-md" required="">
       <span class="help-block">Separados por ( ; )</span>
       </div>
     </div>
@@ -59,12 +83,13 @@
     <div class="form-group">
       <label class="col-md-4 control-label" for="select_ano">Ano</label>
       <div class="col-md-2">
-        <select id="select_ano" name="select_ano" class="form-control">
+        <select id="ano" name="ano" class="form-control">
           <option value="2012">2012</option>
           <option value="2013">2013</option>
           <option value="2014">2014</option>
           <option value="2015">2015</option>
           <option value="2016">2016</option>
+		  <option value="2016">2017</option>
         </select>
       </div>
     </div>
@@ -73,7 +98,7 @@
     <div class="form-group">
       <label class="col-md-4 control-label" for="input_link">Link de arquivos</label>
       <div class="col-md-4">
-      <input id="input_link" name="input_link" type="text" placeholder="insira aqui o link de arquivos complementares" class="form-control input-md">
+      <input id="arquivos" name="arquivos" type="text" placeholder="insira aqui o link de arquivos complementares" class="form-control input-md">
       <span class="help-block">*se tiver arquivos complementares</span>
       </div>
     </div>
@@ -82,18 +107,18 @@
     <div class="form-group">
       <label class="col-md-4 control-label" for="textarea_resumo">Resumo</label>
       <div class="col-md-4">
-        <textarea class="form-control" id="textarea_resumo" name="textarea_resumo">Resumo até 300 palavras.</textarea>
+        <textarea class="form-control" id="resumo" name="resumo" placeholder="Resumo até 300 palavras"></textarea>
       </div>
     </div>
 
     <!-- File Button inserir arquivos-->
     <div class="form-group">
-      <label class="col-md-4 control-label" for="button_arquivos">Anexar relatório</label>
+      <label class="col-md-4 control-label" for="button_arquivos" >Anexar Relatório</label>
       <div class="col-md-4">
-        <input id="button_arquivos" name="button_arquivos" class="input-file" type="file">
+        <input type='file' id='arquivo1' name='arquivo1' required="">
       </div>
     </div>
-
+	
     <!-- Button Cadastro-->
     <div class="form-group">
       <label class="col-md-4 control-label" for="button_cadastrar"></label>
@@ -104,8 +129,6 @@
 
     </fieldset>
     </form>
-
-
 
   </body>
 </html>
---
+++ backend/salvarCadastro.php
@@ -0,0 +1,60 @@
+<?php
+
+		require_once "../banco/conecta.php";
+		date_default_timezone_set('America/Sao_Paulo');
+	
+		conecta_banco();
+		
+		$_POST['titulo'	     ] = addslashes(trim($_POST['titulo'	  ]));
+		$_POST['autores'	 ] = addslashes(trim($_POST['autores'	  ]));
+		$_POST['orientadores'] = addslashes(trim($_POST['orientadores']));
+		$_POST['palavrachave'] = addslashes(trim($_POST['palavrachave']));
+		$_POST['ano'		 ] = addslashes(trim($_POST['ano'		  ]));
+		$_POST['resumo'	 	 ] = addslashes(trim($_POST['resumo'	  ]));
+		$_POST['arquivos'	 ] = addslashes(trim($_POST['arquivos'	  ]));
+		
+		$fileName1 = pathinfo(basename($_FILES["arquivo1"]["name"]),PATHINFO_FILENAME);
+		$fileType1 = pathinfo(basename($_FILES["arquivo1"]["name"]),PATHINFO_EXTENSION);
+		
+		//colocar data e hora no anexo
+		//$_POST['anexo1'] = $fileName1.'_'.date('d_m_Y_H_i_s').'.'.$fileType1;
+		//$_POST['anexo2'] = $fileName2.'_'.date('d_m_Y_H_i_s').'.'.$fileType2;
+		
+		$_POST['anexo'] = $fileName1.'.'.$fileType1;
+		
+		$query = "insert into ppi.projeto (titulo, autores, orientadores, palavrachave, ano, resumo, arquivos, anexo) values
+		        ('{$_POST['titulo']}', '{$_POST['autores']}', '{$_POST['orientadores']}', '{$_POST['palavrachave']}', '{$_POST['ano']}', '{$_POST['resumo']}', '{$_POST['arquivos']}', '{$_POST['anexo']}')";
+		//file_put_contents("log.txt",$query);
+		if(!$ref=mysql_query($query)) {
+			die("Não foi possível executar a instrução MySQL: <br><br>{$query}");
+		}		
+		
+		$last_id = mysql_insert_id(); 
+		
+		$_FILES['arquivo1']['novoNome'] = $_POST['anexo'];
+		
+		$target_dir = "Anexo/";
+		$novaPasta = $target_dir.'projeto_'.$last_id;
+		foreach ($_FILES as $key => $value) {
+			$target_file = basename($_FILES[$key]["name"]);
+			$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+			$fileName = pathinfo($target_file,PATHINFO_FILENAME);
+			$target_file = rtrim($target_file,'.'.$fileType);
+
+			#tamanho do arquivo
+
+			if (!file_exists($novaPasta)){
+			  mkdir($novaPasta);
+			  chmod($novaPasta, 0777);
+			}
+
+			$newFile = $_FILES[$key]['novoNome'];
+			// echo $newFile.'<br>';
+
+			if(!move_uploaded_file($_FILES[$key]["tmp_name"], $novaPasta.'/'.$newFile)){
+			  return 0;
+			}
+		}
+		
+		mysql_close(); 
+		header('location:cadastro.php?status=1');
