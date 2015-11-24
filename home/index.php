5f099ea07f37c06f51a1cd7b1b10f3313c2e1a2e
Frederico Augusto Willig <fredewillig@gmail.com>
2015-11-24 20:12:59 -0200

    mapa & search
    - redução img mapa;
    - alterado endereço img mapa;
    - search orientado a direita da navbar;

--- home/index.php
+++ home/index.php
@@ -19,24 +19,17 @@
 			<nav class="navbar navbar-default" role="navigation">
 			
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button> <a class="navbar-brand" href="#">Brand</a>
+				<a class="navbar-brand" href="index.php">RD3PI</a>
 			</div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li>
-						<a href="#">Link</a>
+						<a href="projeto.php">O projeto</a>
 					</li>
-					<li>
-						<a href="#">Link</a>
-					</li>
+					
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Categorias<strong class="caret"></strong></a>
 						<ul class="dropdown-menu">
 							<li>
 								<a href="#">Action</a>
@@ -61,21 +54,18 @@
 					</li>
 				</ul>
 				
-				<form class="navbar-form navbar-left" role="search">
+				<form class="navbar-form navbar-right" role="search">
 					<div class="form-group">
 						<input type="text" class="form-control">
-							<button type="submit" class="btn btn-default btn-primary right-block">
+							<button type="submit" class="btn btn-default btn-primary">
 								Pesquisar
 							</button>
 					</div>
 				</form>
-
 			</div>
 		</nav>
 		
-		
-		
-      </div>
+		</div>
     </div>
 	
 	
@@ -83,22 +73,21 @@
 		
 		<div class="col-md-12">
 		
-			<img id="Image-Maps-Com-mapa-categorias" src="http://www.image-maps.com/m/private/64691/68530-b2db8o1hsgctu8etvl79m8i9t6_roda_links.png" border="0" width="2095" height="2096" orgWidth="2095" orgHeight="2096" usemap="#mapa-categorias" alt="" />
-			<map name="mapa-categorias" id="ImageMapsCom-mapa-categorias">
-				<area  alt="" title="Projetos" href="http://www.image-maps.com/" shape="poly" coords="1045,381,1045,2,854,20,659,76,489,163,325,293,181,460,83,636,36,774,10,914,2,1047,389,1049,425,820,498,675,630,528,779,438,907,396" style="outline:none;" target="_self"     />
-				<area  alt="" title="Eventos" href="http://www.image-maps.com/" shape="poly" coords="1050,382,1051,2,1308,34,1508,107,1657,199,1777,296,1919,474,2019,654,2077,858,2092,1046,1699,1045,1667,839,1581,666,1450,525,1325,446,1181,397" style="outline:none;" target="_self"     />
-				<area  alt="" title="Mostras" href="http://www.image-maps.com/" shape="poly" coords="1698,1050,2093,1051,2069,1273,2026,1418,1959,1564,1856,1710,1724,1846,1579,1951,1385,2039,1204,2083,1050,2094,1052,1700,1242,1670,1411,1588,1551,1458,1656,1264,1688,1154" style="outline:none;" target="_self"     />
-				<area  alt="" title="Dissertações" href="http://www.image-maps.com/" shape="poly" coords="1045,1699,1044,2094,863,2076,679,2029,511,1947,346,1825,239,1713,153,1592,66,1414,24,1269,6,1146,2,1050,389,1052,415,1219,490,1387,611,1538,733,1618,891,1682" style="outline:none;" target="_self"     />
-			</map>
+			<img id="Image-Maps-Com-mapa-categorias" src="../img/roda_link.png" border="0" width="628" height="628" orgWidth="628" orgHeight="628" usemap="#mapa-categorias" alt="" class="img-responsive center-block"/>
 			
-
-<!--################################################		
-			Imagem original
-			<img alt="Links de Navegação" src="../img/roda_links.png" id="roda_link" class="img-responsive center-block tamanho">       
-#################################################-->
-		</div>
+			<map name="mapa-categorias" id="mapa-categorias">
+				<area id="filtros" alt="Página de filtros de pesquisa" title="Projetos PPI" href="filtro.php" shape="poly" coords="116,312,3,310,4,260,21,198,57,136,96,80,135,55,164,37,199,21,250,6,311,0,310,114,262,120,211,142,172,175,135,228,120,276" style="outline:none;" target="_self"  onmouseover="if(document.images) document.getElementById('Image-Maps-Com-image-maps-2015-11-24-162903').src= '';"   />
+				<area id="eventos" alt="Página de eventos" title="Eventos" href="eventos.php" shape="poly" coords="316,114,318,0,361,2,396,11,452,32,511,69,557,116,596,176,616,225,625,270,627,311,511,309,503,259,472,197,439,160,398,135,353,118" style="outline:none;" target="_self"     />
+				<area id="mostras" alt="Página de mostras de projetos" title="Página de mostras de projetos" href="mostras.php" shape="poly" coords="627,316,620,380,602,437,575,491,536,535,491,572,444,600,379,622,316,628,316,509,371,500,422,476,470,428,496,384,508,316" style="outline:none;" target="_self"     />
+				<area id="dissertacoes" alt="Página de dissertações" title="Página de dissertações" href="dissertacoes.php" shape="poly" coords="0,316,116,316,124,366,144,412,173,452,212,482,256,501,313,509,312,628,260,623,204,608,136,573,92,536,46,476,20,426,4,370" style="outline:none;" target="_self"     />
+			</map>
 		
-    </div>
+		
+		<!-- Imagem Original ################
+		<img alt="Links de Navegação" src="../img/roda_link.png" id="roda_link" class="img-responsive center-block tamanho">
+		-->
+		</div>
+	</div>
     
     <div class="row" id="nomobile">
 		<div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
