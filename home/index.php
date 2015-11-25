27dc07622de6b95f39502db5a2d25044cf95cfb5
fredewillig <fredewillig@gmail.com>
2015-11-25 13:18:11 -0200

    Correção Brand

--- home/index.php
+++ home/index.php
@@ -24,8 +24,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
-						</button> <a class="navbar-brand" href="#">Brand</a>
-						<a class="navbar-brand bg-primary" href="index.php" id="titulo">RD3PI</a>
+						</button> <a class="navbar-brand bg-primary" href="index.php" id="titulo">RD3PI</a>
 					</div>
 	
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
