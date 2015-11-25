c668de4da1f614f3f4b217f0326d297f28619c81
Frederico Augusto Willig <fredewillig@gmail.com>
2015-11-25 02:30:58 -0200

    css & dados projeto

--- home/projeto.php
+++ home/projeto.php
@@ -17,57 +17,55 @@
 		?>
 
 	<body>
+		<div class="container">
+			<div class="col-md-offset-2">
+				<div class="col-md-10">
+					<form id='form1' class="form-horizontal" action="salvarCadastro.php" method="POST" enctype="multipart/form-data">
+					
+						<!-- Resultado de Cadastro de Projetos -->
+						<legend>Dados do projeto</legend>
 			
-		<form id='form1' class="form-horizontal well" action="salvarCadastro.php" method="POST" enctype="multipart/form-data">
-		<fieldset>
-			<!-- Resultado de Cadastro de Projetos -->
-			<legend>Dados do projeto</legend>
-
-			
-				<!-- ######   Tabela de resultados   ###### -->
-				<div class="col-md-1 row-clearfix"></div>
-				<div class="col-md-10 row-clearfix">
-					<table class="table">
-						<thead>
-							<tr>
-								<th>Título do Projeto</th>
-								<td><em class="glyphicon glyphicon-superscript blue"></em> <?=$row[titulo]?></td>
-							</tr>
-							<tr>
-								<th>Autores</th>
-								<td><em class="glyphicon glyphicon-edit blue"></em> <?=$row[autores]?></td>
-							</tr>
-							<tr>
-								<th>Orientadores</th>
-								<td><em class="glyphicon glyphicon-comment blue"></em> <?=$row[orientadores]?></td>
-							</tr>
-							<tr>
-								<th>Palavras-chave</th>
-								<td><em class="glyphicon glyphicon-flag blue"></em> <?=$row[palavrachave]?></td>
-							</tr>
-							<tr>
-								<th>Ano</th>
-								<td><em class="glyphicon glyphicon-calendar blue"></em> <?=$row[ano]?></td>
-							</tr>
-							<tr>
-								<th>Link de arquivos</th>
-								<td><em class="glyphicon glyphicon-file blue"></em> <?=$row[arquivos]?></td>
-							</tr>
-							<tr>
-								<th>Resumo</th>
-								<td><em class="glyphicon glyphicon-pencil blue"></em> <?=$row[resumo]?></td>
-							</tr>
-							<tr>
-								<th>Anexos</th>
-								<td><em class="glyphicon glyphicon-paperclip blue"></em> <a href='<?="../backend/Anexo/projeto_$_GET[id]/$row[anexo]";?>' download><?=$row[anexo]?></a></td>
-							</tr>
-						</thead>
-					</table>
+							<!-- ######   Tabela de resultados   ###### -->
+							
+								<table class="table">
+									<thead>
+										<tr>
+											<th>Título do Projeto</th>
+											<td><em class="glyphicon glyphicon-superscript"></em> <?=$row[titulo]?></td>
+										</tr>
+										<tr>
+											<th>Autores</th>
+											<td><em class="glyphicon glyphicon-edit"></em> <?=$row[autores]?></td>
+										</tr>
+										<tr>
+											<th>Orientadores</th>
+											<td><em class="glyphicon glyphicon-comment"></em> <?=$row[orientadores]?></td>
+										</tr>
+										<tr>
+											<th>Palavras-chave</th>
+											<td><em class="glyphicon glyphicon-flag"></em> <?=$row[palavrachave]?></td>
+										</tr>
+										<tr>
+											<th>Ano</th>
+											<td><em class="glyphicon glyphicon-calendar"></em> <?=$row[ano]?></td>
+										</tr>
+										<tr>
+											<th>Link de arquivos</th>
+											<td><em class="glyphicon glyphicon-file"></em> <?=$row[arquivos]?></td>
+										</tr>
+										<tr>
+											<th>Resumo</th>
+											<td><em class="glyphicon glyphicon-pencil"></em><textarea class="form-control" id="textarea" name="textarea"><?=$row[arquivos]?></textarea></td>
+										</tr>
+										<tr>
+											<th>Anexos</th>
+											<td><em class="glyphicon glyphicon-paperclip"></em> <a href='<?="../backend/Anexo/projeto_$_GET[id]/$row[anexo]";?>' download><?=$row[anexo]?></a></td>
+										</tr>
+									</thead>
+								</table>
+					</form>
 				</div>
-				<div class="col-md-1 row-clearfix"></div>
-	
-		</fieldset>
-		</form>
-	
+			</div>
+		</div>
 	</body>
 </html>
